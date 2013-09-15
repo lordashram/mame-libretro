c9f91408285745e6bf34236c00d604e7459b4e8f
twinaphex <libretro@gmail.com>
2013-09-15 16:27:23 +0200

    Make macroed versions out of palette inlined functions

--- src/lib/util/palette.h
+++ src/lib/util/palette.h
@@ -200,11 +200,7 @@ void palette_normalize_range(palette_t *palette, UINT32 start, UINT32 end, int l
     a 15-bit OSD-specified RGB value
 -------------------------------------------------*/
 
-inline rgb15_t rgb_to_rgb15(rgb_t rgb)
-{
-	return ((RGB_RED(rgb) >> 3) << 10) | ((RGB_GREEN(rgb) >> 3) << 5) | ((RGB_BLUE(rgb) >> 3) << 0);
-}
-
+#define rgb_to_rgb15(rgb) (((RGB_RED(rgb) >> 3) << 10) | ((RGB_GREEN(rgb) >> 3) << 5) | ((RGB_BLUE(rgb) >> 3) << 0))
 
 /*-------------------------------------------------
     rgb_clamp - clamp an RGB component to 0-255
@@ -224,77 +220,47 @@ inline UINT8 rgb_clamp(INT32 value)
     pal1bit - convert a 1-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal1bit(UINT8 bits)
-{
-	return (bits & 1) ? 0xff : 0x00;
-}
+#define pal1bit(bits) ((bits & 1) ? 0xff : 0x00)
 
 
 /*-------------------------------------------------
     pal2bit - convert a 2-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal2bit(UINT8 bits)
-{
-	bits &= 3;
-	return (bits << 6) | (bits << 4) | (bits << 2) | bits;
-}
-
+#define pal2bit(bits) ({UINT8 bit = bits & 3; ((bit << 6) | (bit << 4) | (bit << 2) | bit);})
 
 /*-------------------------------------------------
     pal3bit - convert a 3-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal3bit(UINT8 bits)
-{
-	bits &= 7;
-	return (bits << 5) | (bits << 2) | (bits >> 1);
-}
-
+#define pal3bit(bits) ({UINT8 bit = bits & 7; ((bit << 5) | (bit << 2) | (bit >> 1));})
 
 /*-------------------------------------------------
     pal4bit - convert a 4-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal4bit(UINT8 bits)
-{
-	bits &= 0xf;
-	return (bits << 4) | bits;
-}
+#define pal4bit(bits) ({UINT8 bit = bits & 0xf; ((bit << 4) | bit);})
 
 
 /*-------------------------------------------------
     pal5bit - convert a 5-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal5bit(UINT8 bits)
-{
-	bits &= 0x1f;
-	return (bits << 3) | (bits >> 2);
-}
+#define pal5bit(bits) ({UINT8 bit = bits & 0x1f; ((bit << 3) | (bit >> 2));})
 
 
 /*-------------------------------------------------
     pal6bit - convert a 6-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal6bit(UINT8 bits)
-{
-	bits &= 0x3f;
-	return (bits << 2) | (bits >> 4);
-}
+#define pal6bit(bits) ({UINT8 bit = bits & 0x3f; ((bit << 2) | (bit >> 4));})
 
 
 /*-------------------------------------------------
     pal7bit - convert a 7-bit value to 8 bits
 -------------------------------------------------*/
 
-inline UINT8 pal7bit(UINT8 bits)
-{
-	bits &= 0x7f;
-	return (bits << 1) | (bits >> 6);
-}
-
+#define pal7bit(bits) ({UINT8 bit = bits & 0x7f; ((bit << 1) | (bit >> 6));})
 
 //-------------------------------------------------
 //  palexpand - expand a palette value to 8 bits
@@ -319,54 +285,35 @@ inline UINT8 palexpand(UINT8 data)
     bits from a UINT32
 -------------------------------------------------*/
 
-inline rgb_t pal332(UINT32 data, UINT8 rshift, UINT8 gshift, UINT8 bshift)
-{
-	return MAKE_RGB(pal3bit(data >> rshift), pal3bit(data >> gshift), pal2bit(data >> bshift));
-}
-
+#define pal332(data, rshift, gshift, bshift) (MAKE_RGB(pal3bit(data >> rshift), pal3bit(data >> gshift), pal2bit(data >> bshift)))
 
 /*-------------------------------------------------
     pal444 - create a 4-4-4 color by extracting
     bits from a UINT32
 -------------------------------------------------*/
 
-inline rgb_t pal444(UINT32 data, UINT8 rshift, UINT8 gshift, UINT8 bshift)
-{
-	return MAKE_RGB(pal4bit(data >> rshift), pal4bit(data >> gshift), pal4bit(data >> bshift));
-}
-
+#define pal444(data, rshift, gshift, bshift) (MAKE_RGB(pal4bit(data >> rshift), pal4bit(data >> gshift), pal4bit(data >> bshift)))
 
 /*-------------------------------------------------
     pal555 - create a 5-5-5 color by extracting
     bits from a UINT32
 -------------------------------------------------*/
 
-inline rgb_t pal555(UINT32 data, UINT8 rshift, UINT8 gshift, UINT8 bshift)
-{
-	return MAKE_RGB(pal5bit(data >> rshift), pal5bit(data >> gshift), pal5bit(data >> bshift));
-}
-
+#define pal555(data, rshift, gshift, bshift) (MAKE_RGB(pal5bit(data >> rshift), pal5bit(data >> gshift), pal5bit(data >> bshift)))
 
 /*-------------------------------------------------
     pal565 - create a 5-6-5 color by extracting
     bits from a UINT32
 -------------------------------------------------*/
 
-inline rgb_t pal565(UINT32 data, UINT8 rshift, UINT8 gshift, UINT8 bshift)
-{
-	return MAKE_RGB(pal5bit(data >> rshift), pal6bit(data >> gshift), pal5bit(data >> bshift));
-}
-
+#define pal565(data, rshift, gshift, bshift) (MAKE_RGB(pal5bit(data >> rshift), pal6bit(data >> gshift), pal5bit(data >> bshift)))
 
 /*-------------------------------------------------
     pal888 - create a 8-8-8 color by extracting
     bits from a UINT32
 -------------------------------------------------*/
 
-inline rgb_t pal888(UINT32 data, UINT8 rshift, UINT8 gshift, UINT8 bshift)
-{
-	return MAKE_RGB(data >> rshift, data >> gshift, data >> bshift);
-}
+#define pal888(data, rshift, gshift, bshift) (MAKE_RGB(data >> rshift, data >> gshift, data >> bshift))
 
 
 #endif  /* __PALETTE_H__ */
