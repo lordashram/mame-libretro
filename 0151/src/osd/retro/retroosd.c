7aa35826dc314ff04ea520b4febbcacdbf6603d3
George Kranis <g.kranis@server.fake>
2013-11-30 07:30:50 +0200

    correct core PAR reporting,cosmetics

--- 0151/src/osd/retro/retroosd.c
+++ 0151/src/osd/retro/retroosd.c
@@ -52,6 +52,11 @@ void mini_osd_interface::init(running_machine &machine)
         
 	prep_retro_rotation(gamRot);
 	our_target->compute_minimum_size(rtwi, rthe);
+	topw=rtwi;
+	//Equivalent to rtaspect=our_target->view_by_index((our_target->view()))->effective_aspect(render_layer_config layer_config())
+	int width,height;
+	our_target->compute_visible_area(1000,1000,1,ROT0,width,height);
+	rtaspect=(float)width/(float)height;
 
 	write_log("osd init done\n");
 }
