b352f108f2fa3a653808d3c85baa97655d44f1f6
r-type <r-type@users.noreply.github.com>
2014-03-30 16:05:42 +0200

    refactor and cleanup

--- 0152/src/osd/retro/retroexpcmd.c
+++ 0152/src/osd/retro/retroexpcmd.c
@@ -1,6 +1,31 @@
-//Args for CmdLine used if experimental_cmdline enabled.
+//Args for Core
+static char XARGV[64][1024];
+static const char* xargv_cmd[64];
+
+//Args for experimental_cmdline
 static char ARGUV[32][1024];
 static char ARGUC=0;
+
+#define NB_OPTPATH 12
+
+static const char *dir_name[NB_OPTPATH]={
+	"cfg","nvram","memcard","input",
+	"states" ,"snaps","diff","samples",
+	"artwork","cheat","ini","hash"
+};
+
+static const char *opt_name[NB_OPTPATH]={
+	"-cfg_directory","-nvram_directory","-memcard_directory","-input_directory",
+	"-state_directory" ,"-snapshot_directory","-diff_directory","-samplepath",
+	"-artpath","-cheatpath","-inipath","-hashpath"
+};
+
+//0 save_dir | 1 system_dir
+int opt_type[NB_OPTPATH]={
+	0,0,0,0,
+	0,0,0,1,
+	1,1,1,1
+};
 
 void parse_cmdline( const char *argv ){
 
@@ -59,157 +84,98 @@ void parse_cmdline( const char *argv ){
 	
 }
 
-
 int executeGame_cmd(char* path) {
 
-	// lot of redundant code of executeGame
+	char tmp_dir[256];
+
 	int paramCount;
 	int result = 0;
 	int gameRot=0;
 	int driverIndex;
+	bool CreateConf = ( strcmp(ARGUV[0],"-cc") == 0 || strcmp(ARGUV[0],"-createconfig") == 0 )?1:0;
+	bool Only1Arg =   ( ARGUC==1 )?1:0;
 
-	FirstTimeUpdate = 1;
-	
 	FirstTimeUpdate = 1;
 
 	screenRot = 0;
 
-	//MAME 1 args -> arcade rom with full path
-	if(ARGUC==1){ 
-		//split the path to directory and the name without the zip extension
-		result = parsePath(path, MgamePath, MgameName);
-		if (result == 0) {
-			write_log("parse path failed! path=%s\n", path);
-			strcpy(MgameName,path );
-		}
-		//Find the game info. Exit if game driver was not found.
-		if (getGameInfo(MgameName, &gameRot, &driverIndex) == 0) {
-			write_log("game not found: %s\n", MgameName);
-			return -2;
-		}
+	for (int i = 0; i<64; i++)xargv_cmd[i]=NULL;
+
+	//split the path to directory and the name without the zip extension
+	if (parsePath(Only1Arg?path:ARGUV[ARGUC-1], MgamePath, MgameName) == 0) {
+		write_log("parse path failed! path=%s\n", path);
+		strcpy(MgameName,path );
 	}
-	else {  //MESS
-		//split the path to directory and the name without the zip extension
-		result = parsePath(ARGUV[ARGUC-1], MgamePath, MgameName);
-		if (result == 0) {
-			write_log("parse path failed! path=%s\n", path);
-			strcpy(MgameName,path );
+	//Find the game info. Exit if game driver was not found.
+	if (getGameInfo(Only1Arg?MgameName:ARGUV[0], &gameRot, &driverIndex) == 0) {
+
+		// handle -cc/-createconfig case
+		if(CreateConf){
+			write_log("create an %s config\n", core);
 		}
-		//Find the game info. Exit if game driver was not found.
-		if (getGameInfo(ARGUV[0], &gameRot, &driverIndex) == 0) {
+		else {
 			write_log("game not found: %s\n", MgameName);
 			return -2;
 		}
 	}
 
-	write_log("creating frontend... game=%s\n", MgameName);
+	//some hardcoded default Options
+	paramCount=0;
+	sprintf(XARGV[paramCount++],"%s\0",core);
+	sprintf(XARGV[paramCount++],"%s\0","-joystick");
+	sprintf(XARGV[paramCount++],"%s\0","-samplerate");
+	sprintf(XARGV[paramCount++],"%s\0","48000");
+	sprintf(XARGV[paramCount++],"%s\0","-sound");
+	sprintf(XARGV[paramCount++],"%s\0","-cheat");
+        sprintf(XARGV[paramCount++],"%s\0","-mouse");
 
-	//find how many parameters we have
-	for (paramCount = 0; xargv[paramCount] != NULL; paramCount++)
-		printf("args: %s\n",xargv[paramCount]);
+	//Setup path Option according to retro (save/system) directory or current if NULL 
+	for(int i=0;i<NB_OPTPATH;i++){
 
-	if(ARGUC==1){// 1 args -> arcade rom with full path  
-			xargv[paramCount++] = (char*)"-rp";	
-			xargv[paramCount++] = (char*)g_rom_dir;	
+		sprintf(XARGV[paramCount++],"%s\0",(char*)(opt_name[i]));
+
+		if(opt_type[i]==0){
+			if(retro_save_directory!=NULL)sprintf(tmp_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash,dir_name[i]);	
+			else sprintf(tmp_dir, "%s%c%s%c%s", ".", slash, core, slash,dir_name[i]);
+		}
+		else {
+			if(retro_system_directory!=NULL)sprintf(tmp_dir, "%s%c%s%c%s", retro_system_directory, slash, core, slash,dir_name[i]);	
+			else sprintf(tmp_dir, "%s%c%s%c%s", ".", slash, core, slash,dir_name[i]);
+		}
+
+		sprintf(XARGV[paramCount++],"%s\0",(char*)(tmp_dir));
 	}
 
-	xargv[paramCount++] = (char*)("-cfg_directory");
-	
-	char cfg_dir[256];
-	sprintf(cfg_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "cfg");
-	xargv[paramCount++] = (char*)(cfg_dir);
-	
-	xargv[paramCount++] = (char*)("-nvram_directory");
-	
-	char nv_dir[256];
-	sprintf(nv_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "nvram");
-	xargv[paramCount++] = (char*)(nv_dir);
-	
-	xargv[paramCount++] = (char*)("-memcard_directory");
-	
-	char mem_dir[256];
-	sprintf(mem_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "memcard");
-	xargv[paramCount++] = (char*)(mem_dir);
-		
-	xargv[paramCount++] = (char*)("-input_directory");
-	
-	char inp_dir[256];
-	sprintf(inp_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "input");
-	xargv[paramCount++] = (char*)(inp_dir);
-	
-	xargv[paramCount++] = (char*)("-state_directory");
-	
-	char state_dir[256];
-	sprintf(state_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "states");
-	xargv[paramCount++] = (char*)(state_dir);
-		
-	xargv[paramCount++] = (char*)("-snapshot_directory");
-	
-	char snap_dir[256];
-	sprintf(snap_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "snaps");
-	xargv[paramCount++] = (char*)(snap_dir);
-		
-	xargv[paramCount++] = (char*)("-diff_directory");
+	if(Only1Arg){// Assume arcade rom with full path or -cc  
 
-	char diff_dir[256];
-	sprintf(diff_dir, "%s%c%s%c%s", retro_save_directory, slash, core, slash, "diff");
-	xargv[paramCount++] = (char*)(diff_dir);
-	
-	xargv[paramCount++] = (char*)("-samplepath");
-	
-	char samples_dir[256];
-	sprintf(samples_dir, "%s%c%s%c%s", retro_system_directory, slash, core, slash, "samples");
-	xargv[paramCount++] = (char*)(samples_dir);
-	
-	xargv[paramCount++] = (char*)("-artpath");
-	
-	char art_dir[256];
-	sprintf(art_dir, "%s%c%s%c%s", retro_system_directory, slash, core, slash, "artwork");
-	xargv[paramCount++] = (char*)(art_dir);
-		
-	xargv[paramCount++] = (char*)("-cheatpath");
-	
-	char cheat_dir[256];
-	sprintf(cheat_dir, "%s%c%s%c%s", retro_system_directory, slash, core, slash, "cheat");
-	xargv[paramCount++] = (char*)(cheat_dir);
-	
-	xargv[paramCount++] = (char*)("-inipath");
-	
-	char ini_dir[256];
-	sprintf(ini_dir, "%s%c%s%c%s", retro_system_directory, slash, core, slash, "ini");
-	xargv[paramCount++] = (char*)(ini_dir);	
-	xargv[paramCount++] = (char*)("-hashpath");
-	
-	char hash_dir[256];
-	sprintf(hash_dir, "%s%c%s%c%s", retro_system_directory, slash, core, slash, "hash");
-	xargv[paramCount++] = (char*)(hash_dir);				
-
-
-        xargv[paramCount++] = (char*)"-mouse";
-
-	//MAME 1 args -> arcade rom with full path
-	if(ARGUC==1){
-		xargv[paramCount++] = MgameName;
+		if(CreateConf)
+			sprintf(XARGV[paramCount++],"%s\0",(char*)"-createconfig");
+		else {
+			sprintf(XARGV[paramCount++],"%s\0",(char*)"-rp");	
+			sprintf(XARGV[paramCount++],"%s\0",(char*)g_rom_dir);	
+			sprintf(XARGV[paramCount++],"%s\0",MgameName);
+		}
 	}
-	else { //MESS  pass all cmdline args
+	else { // Pass all cmdline args
 		for(int i=0;i<ARGUC;i++)
-			xargv[paramCount++] = ARGUV[i];
+			sprintf(XARGV[paramCount++],"%s\0", ARGUV[i]);
 	}
 
 	write_log("frontend parameters:%i\n", paramCount);
 
-	for (int i = 0; xargv[i] != NULL; i++){
-		write_log("  %s\n",xargv[i]);
+	for (int i = 0; i<paramCount; i++){
+		xargv_cmd[i] = (char*)(XARGV[i]);
+		write_log("  %s\n",XARGV[i]);
 	}
-	
+
 	osd_init_midi();
 
 	cli_options MRoptions;
 	mini_osd_interface MRosd;
 	cli_frontend frontend(MRoptions, MRosd);
-	result = frontend.execute(paramCount, ( char **)xargv); 
+	result = frontend.execute(paramCount, ( char **)xargv_cmd); 
 
-	xargv[paramCount - 2] = NULL;
+	xargv_cmd[paramCount - 2] = NULL;
+
 	return result;
 }
-
